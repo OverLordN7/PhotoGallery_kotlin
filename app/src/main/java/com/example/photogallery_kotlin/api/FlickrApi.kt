07f6471d5e5a416da086fd3c3e6e4f65c78e834b
Sarvar <arifdjanovsarvar@gmail.com>
2022-11-07 16:45:47 +0500

    phase 6
    Search integration

--- app/src/main/java/com/example/photogallery_kotlin/api/FlickrApi.kt
+++ app/src/main/java/com/example/photogallery_kotlin/api/FlickrApi.kt
@@ -4,18 +4,18 @@ import com.example.photogallery_kotlin.FlickrResponse
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 import retrofit2.http.Url
 
 
 interface FlickrApi {
 
-    @GET("services/rest/?method=flickr.interestingness.getList"+
-            "&api_key=f192d06238a081a8530c92d57cb668b8" +
-            "&format=json" +
-            "&nojsoncallback=1" +
-            "&extras=url_s")
+    @GET("services/rest?method=flickr.interestingness.getList")
     fun fetchPhotos(): Call<FlickrResponse>
 
     @GET
     fun fetchUrlBytes(@Url url: String): Call<ResponseBody>
+
+    @GET("services/rest?method=flickr.photos.search")
+    fun searchPhotos(@Query("text") query: String): Call<FlickrResponse>
 }
